37cddcdfa20de0dd6256597a1c0d9062028194bc
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-08-19 22:00:43 -0300

    finish class 3

--- Class_3/app.py
+++ Class_3/app.py
@@ -1,8 +1,22 @@
-from flask import Flask , render_template,request ,redirect
+from flask import Flask , render_template,request ,redirect,session
 
 #instanciando
 app = Flask(__name__,template_folder='./templates')
 
+app.secret_key = "hadouken"
+
+class Pessoa:
+  def __init__(self,nome:str,cargo:str,stacks:str) -> None:
+    self.nome = nome
+    self.cargo = cargo
+    self.stacks = stacks 
+    
+
+pessoa1 = Pessoa("Johnny","Full stack", "Python, JS")
+pessoa2 = Pessoa("Fulano","Back End", "Python")
+pessoa3 = Pessoa("Sicrana","Front End", " JS")
+
+lista_pesssoas = [ pessoa1,pessoa2,pessoa3 ]
 
 
 @app.route("/", methods = ['GET',])
@@ -10,12 +24,10 @@ def home():
   return render_template("index.html", pessoas = lista_pesssoas)
 
 @app.route("/cadastro/")
-#render
 def cadastrar():
   return render_template("cadastrar_time.html")
 
 @app.route("/adicionar", methods = ['POST',])
-#função para cadastrar
 def cadastrarPessoas():
   nome = request.form["nome"]
   cargo = request.form["cargo"]
@@ -28,15 +40,13 @@ def cadastrarPessoas():
   return redirect("/")
 
 @app.route("/login", methods = ['get'])
-
 def login():
-
   return render_template("login.html",)
 
 @app.route("/autenticar", methods = ['POST',])
-
 def autenticar():
 
+  #seria armazenado na DB
   usuariodefault = "admin"
   senhadefault = "senha123"
 
@@ -44,18 +54,21 @@ def autenticar():
   senha = request.form["senha"]
 
   if login == usuariodefault and senhadefault == senha :
+    session["usuario_logado"] = request.form[login]
+
     return redirect("/")
   else:
     return redirect("/falha")
   
 @app.route("/falha",)
-
 def falha():
   return render_template("falha.html")
     
 
-
-
+@app.route("/logout")
+def logout():
+  session["usuario_logado"] = None
+  return redirect("/login")
 
 
 @app.errorhandler(404)
